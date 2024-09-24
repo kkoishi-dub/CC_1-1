9cfa7cb149be10192d7fccf1cd46b56a2737a62c
kkoishi-dub <kkoishi@amazon.com>
2024-09-24 14:37:13 +0100

    Update script.js
    Add number checker

--- script.js
+++ script.js
@@ -4,14 +4,20 @@
 
   const form = document.getElementById('convert');
   const converter = 1.60934;
-
+  const checkIfNumber = /^[-]?([1-9]\d*|0)(\.\d+)?$/;
   form.addEventListener('submit', function(event) {
     event.preventDefault();
     const element = document.querySelector('#answer').querySelector('h2'); 
-    const distance = document.getElementById('distance').value
+    const distance = document.getElementById('distance').value;
     element.classList.remove('invisible');
-    element.innerHTML = `${distance} miles converts to ${distance * converter} kilometers`;
 
+    if(checkIfNumber.test(distance)){
+      element.innerHTML = `${distance} miles converts to ${distance * converter} kilometers`;
+    }else{
+      element.innerHTML = "(Error) Please input the correct Number";
+    }
+      
+    
   });
 
 
